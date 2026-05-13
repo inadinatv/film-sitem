0407ba46b91cb47b8839763867a6e7d6bffbe454
inadinatv <aztechno.72@gmail.com>
2026-05-13 22:24:13 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -51,7 +51,7 @@ if (file_exists($jsonFile)) {
 }
 
 // 2. GÜVENLİ LİMİT: Siteyi şüphelendirmeden kategori başı en güncel 3 sayfayı çeker
-$max_page_limit = 600; 
+$max_page_limit = 900; 
 
 foreach ($categories as $path => $catName) {
     $i = 1; 
